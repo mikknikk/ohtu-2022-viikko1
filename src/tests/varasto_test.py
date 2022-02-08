32129ff8a0d6162419280b996c2b6a8be225e095
mikknikk <mikko.nikkila@iki.fi>
2022-02-08 21:25:14 +0200

    Päivitetään

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -38,3 +38,52 @@ class TestVarasto(unittest.TestCase):
 
         # varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 4)
+
+    def test_yritetaan_ottaa_liikaa(self):
+        self.varasto.lisaa_varastoon(8)
+
+        self.varasto.ota_varastosta(12)
+
+        # varastossa pitäisi olla 0
+        self.assertAlmostEqual(self.varasto.saldo, 0)
+
+    def test_yritetaan_lisata_liikaa(self):
+        self.varasto.lisaa_varastoon(8)
+
+        self.varasto.lisaa_varastoon(4)
+
+        #varastossa pitäisi olla 10
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    def test_ota_negatiivinen(self):
+        self.varasto.lisaa_varastoon(8)
+
+        self.varasto.ota_varastosta(-6)
+
+        # varastosaldon ei pitäisi muuttua
+        self.assertAlmostEqual(self.varasto.saldo, 8)
+
+    def test_lisaa_negatiivinen(self):
+        self.varasto.lisaa_varastoon(8)
+
+        self.varasto.lisaa_varastoon(-4)
+
+        # varastosaldon ei pitäisi muuttua
+        self.assertAlmostEqual(self.varasto.saldo, 8)
+
+    def test_negatiivinen_varaston_tilavuus(self):
+        varasto = Varasto(-5)
+
+        # tilavuuden pitäisi olla 0
+        self.assertAlmostEqual(varasto.tilavuus, 0)
+
+    def test_negatiivinen_alkusaldo(self):
+        varasto = Varasto(10, -5)
+
+        # varastosaldon pitäisi olla 0
+        self.assertAlmostEqual(varasto.saldo, 0)
+
+    def test_str(self):
+        self.varasto.lisaa_varastoon(8)
+
+        self.assertEqual(self.varasto.__str__(), "saldo = 8, vielä tilaa 2")
